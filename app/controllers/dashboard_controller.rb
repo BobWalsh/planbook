dd7daaadb5317f0bb2e66e5a2288d0b8b85b212a
Bob Walsh <bob.walsh@47hats.com>
2017-01-09 08:32:47 -0800

    Added Vue.js

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
-
+  include DashboardHelper
   def show
     @user = current_user
+    @country = random_country
   end
 end
